eaddfc30f6fffe27a77067a2946721aa78831c6a
DIPURANJAN SETHY <dipuranjansethy1432@gmail.com>
2024-02-14 18:52:06 +0530

    cors added

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import user from './routes/userRoutes.js'
 import payment from './routes/paymentRoutes.js'
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from 'cookie-parser';
-import otehr from './routes/otherRoutes.js';
-
+import other from './routes/otherRoutes.js';
+import cors from 'cors'
 
 
 config({
@@ -15,6 +15,11 @@ config({
 
 const app=express()
 app.use(cookieParser())
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"],
+}))
 
 //using middlewares
 app.use(express.json())
@@ -25,10 +30,14 @@ app.use(
 
 )
 
+
 app.use("/api/v1",course)
 app.use("/api/v1",user)
 app.use("/api/v1",payment)
-app.use("/api/v1",otehr)
+app.use("/api/v1",other)
 
 export default app;
+app.get("/",(req,res)=>{
+    res.send("<h1>site is working click Click<a href=${process.env.FRONT_URL}>Here</a> to visit frontend</h1>")
+})
 app.use(ErrorMiddleware)
