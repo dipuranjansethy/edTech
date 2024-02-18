df1b050ccc56ecdf70acfa0f5328335ac25cfdfe
DIPURANJAN SETHY <dipuranjansethy1432@gmail.com>
2024-02-19 03:11:51 +0530

    stats fixed

--- controllers/otherController.js
+++ controllers/otherController.js
@@ -58,7 +58,7 @@ export const getDashboardStats = catchAsyncError(async (req, res, next) => {
         
     }
     const usersCount = statsData[11].users;
-    const subscriptionCount = statsData[11].subscription;
+    const subscriptionCount = statsData[11].subscriptions;
     const viewsCount = statsData[11].views;
 
     let usersPercentage = 0,
@@ -70,19 +70,19 @@ export const getDashboardStats = catchAsyncError(async (req, res, next) => {
 
   if (statsData[10].users === 0) usersPercentage = usersCount * 100;
   if (statsData[10].views === 0) viewsPercentage = viewsCount * 100;
-  if (statsData[10].subscription === 0)
+  if (statsData[10].subscriptions === 0)
     subscriptionPercentage = subscriptionCount * 100;
   else {
     const difference = {
       users: statsData[11].users - statsData[10].users,
       views: statsData[11].views - statsData[10].views,
-      subscription: statsData[11].subscription - statsData[10].subscription,
+      subscription: statsData[11].subscriptions - statsData[10].subscriptions,
     };
 
     usersPercentage = (difference.users / statsData[10].users) * 100;
     viewsPercentage = (difference.views / statsData[10].views) * 100;
     subscriptionPercentage =
-      (difference.subscription / statsData[10].subscription) * 100;
+      (difference.subscription / statsData[10].subscriptions) * 100;
     if (usersPercentage < 0) usersProfit = false;
     if (viewsPercentage < 0) viewsProfit = false;
     if (subscriptionPercentage < 0) subscriptionProfit = false;
